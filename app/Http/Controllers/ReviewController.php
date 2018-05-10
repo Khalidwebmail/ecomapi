febc7b316602b75508a28fad6bcd8ccc1950c41e
Khalid <csewebmail@gmail.com>
2018-05-11 00:16:55 +0600

    API resource with post man

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Model\Review;
-
+use App\Model\Product;
+use App\Http\Resources\ReviewResource;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
@@ -13,9 +14,9 @@ class ReviewController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Product $product)
     {
-
+        return ReviewResource::collection($product->reviews);
     }
 
     /**
